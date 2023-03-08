1ee0ecc99030f7a9cdcfb3a6fc8d5fc933407d73
Aymard <yakubaba1234@gmail.com>
2023-03-08 13:22:10 +0100

    added the job route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import { userRoute } from './users.js'
+import {jobRoute} from './jobs.js'
 
 export const ROUTES = {
     users: userRoute,
+    jobs: jobRoute,
 }
---
+++ routes/jobs.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+import {getAllJobs, getOneJob, saveJob, deleteJob,updateJob} from "../controllers/jobController.js"
+
+const jobRoute = express.Router()
+
+jobRoute.get("/",getAllJobs)
+jobRoute.get("/:id",getOneJob)
+jobRoute.post("/",saveJob)
+jobRoute.delete("/:id",deleteJob)
+jobRoute.put("/:id",updateJob)
